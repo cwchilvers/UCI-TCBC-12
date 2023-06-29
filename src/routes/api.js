e09eee0fef0eb7ae17d61c5db8c349e50ce8268c
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-28 19:27:01 -0700

    condensed api.js code by moving handler imports into an object

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,47 +1,24 @@
 const app = require('express').Router();  
 
-// Handlers
-const viewEmployees_handler = require('../handlers/viewEmployees_handler');
-const addEmployee_handler = require('../handlers/addEmployee_handler');
-const updateEmployee_handler = require('../handlers/updateEmployee_handler');
-const viewRoles_handler = require('../handlers/viewRoles_handler');
-const addRole_handler = require('../handlers/addRole_handler');
-const viewDepartments_handler = require('../handlers/viewDepartments_handler');
-const addDepartment_handler = require('../handlers/addDepartment_handler');
+// Import handlers in an object
+const handlers = {
+    viewEmployees: require('../handlers/viewEmployees_handler'),
+    addEmployee: require('../handlers/addEmployee_handler'),
+    updateEmployee: require('../handlers/updateEmployee_handler'),
+    viewRoles: require('../handlers/viewRoles_handler'),
+    addRole: require('../handlers/addRole_handler'),
+    viewDepartments: require('../handlers/viewDepartments_handler'),
+    addDepartment: require('../handlers/addDepartment_handler'),
+};
 
-// View all employees
-app.get('/employees', (req, res) => {
-    viewEmployees_handler(req, res);
-});
-
-// Add employee
-app.post('/add-employee', (req, res) => {
-    addEmployee_handler(req, res);
-});
-
-// Update employee role
-app.post('/update-employee', (req, res) => {
-    updateEmployee_handler(req, res);
-});
-
-// View all roles
-app.get('/roles', (req, res) => {
-    viewRoles_handler(req, res);
-});
-
-// Add role
-app.post('/add-role', (req, res) => {
-    addRole_handler(req, res);
-});
-
-// View all departments
-app.get('/departments', (req, res) => {
-    viewDepartments_handler(req, res);
-});
-
-// Add department
-app.post('/add-department', (req, res) => {
-    addDepartment_handler(req, res);
-});
+// Route requests to handlers
+app
+    .get('/employees', handlers.viewEmployees)
+    .post('/add-employee', handlers.addEmployee)
+    .post('/update-employee', handlers.updateEmployee)
+    .get('/roles', handlers.viewRoles)
+    .post('/add-role', handlers.addRole)
+    .get('/departments', handlers.viewDepartments)
+    .post('/add-department', handlers.addDepartment)
 
 module.exports = app;
